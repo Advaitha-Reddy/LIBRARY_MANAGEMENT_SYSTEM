673d302984e7daa24241963925f3163e59bde9c0
Advaitha Patti <157230612+Advaitha-Reddy@users.noreply.github.com>
2026-05-31 18:06:27 +0530

    Delete Member.sql

--- Member.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE Member(
-    member_id INT PRIMARY KEY,
-    member_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE,
-    phone VARCHAR(15) UNIQUE,
-    address VARCHAR(200),
-    membership_date DATE NOT NULL
-);
